6ec806050ffbe3f452943fd335893f8881f350c8
aka kautinkazu <81628831+chris1384@users.noreply.github.com>
2023-06-04 02:44:39 +0200

    added autotas trigger event for default race

--- new/tas/server.lua
+++ new/tas/server.lua
@@ -3,20 +3,22 @@
 		* version 1.4
 --]]
 
---[[
+-- // Use the event given by the Race Default
 addEvent("onRaceStateChanging")
 addEventHandler("onRaceStateChanging", root, function(new)
-	local everyone = getElementsByType("player")
-	if new == "Running" then
-		triggerClientEvent(everyone, "tas:triggerCommand", everyone, "Started")
-	elseif new == "NoMap" or new == "PostFinish" then
-		triggerClientEvent(everyone, "tas:triggerCommand", everyone, "Stop")
-	end
-end)
-]]
 
-addEvent("tas:onModelChange")
+	local everyone = getElementsByType("player")
+	
+	if new == "Running" then
+		triggerClientEvent(everyone, "tas:triggerCommand", resourceRoot, "Started")
+	elseif new == "NoMap" or new == "PostFinish" then
+		triggerClientEvent(everyone, "tas:triggerCommand", resourceRoot, "Stop")
+	end
+	
+end)
+
+-- // Model Change Resync with clients (might break up)
+addEvent("tas:onModelChange", true)
 addEventHandler("tas:onModelChange", root, function(model)
-	local vehicle = source
-	setElementModel(vehicle, model)
+	setElementModel(source, model)
 end)
